fbdb91a95776b1d4a5e20e5df17d09b40579fb03
CosmicGallant <123218665+TeamCherryStalker@users.noreply.github.com>
2024-11-01 20:16:39 -0300

    Update Main.java

--- Quizzy/Quizzy/src/game/Main.java
+++ Quizzy/Quizzy/src/game/Main.java
@@ -5,7 +5,6 @@ public class Main
 	{
 		Questions write = new Questions();
 		Data coisas = new Data(); 
-		//int answer = 0;
 		for(int i = 0; i < coisas.perguntas.length;i++)
 		{
 			write.question(coisas.perguntas[i], coisas.opcoes[i], coisas.opcoes[i+1], coisas.opcoes[i+2], coisas.opcoes[i+3], coisas.opcoes[i+4], 0, coisas.respostas[i]);
