8f392fe69089bebf93850c539bc01c48e661dd5a
CosmicGallant <123218665+TeamCherryStalker@users.noreply.github.com>
2024-11-02 19:18:41 -0300

    Update Main.java

--- Quizzy/Quizzy/src/game/Main.java
+++ Quizzy/Quizzy/src/game/Main.java
@@ -1,6 +1,8 @@
 package game;
+// Classe principal responsável por rodar o quiz.
 public class Main 
 {
+	// Função principal responsável por mostrar tudo no console
 	public static void main(String[] args) 
 	{
 		int n = 0;
