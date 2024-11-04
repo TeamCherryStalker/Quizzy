42c95c29f7f5fa212053ea0642812686b6e3db69
CosmicGallant <123218665+TeamCherryStalker@users.noreply.github.com>
2024-11-04 20:02:12 -0300

    Update Methods.java

--- Quizzy/Quizzy/src/game/Methods.java
+++ Quizzy/Quizzy/src/game/Methods.java
@@ -3,10 +3,10 @@ import java.util.*;
 // Classe responsável por armazena todos os métodos que utilizaremos no projeto
 public class Methods 
 {
+	static Scanner teclado = new Scanner(System.in);
 	// Função responsável criar uma pergunta do quiz e suas respostas
 	public static void question(String text, String answer_1, String answer_2, String answer_3, String answer_4, String answer_5, int number, int right)
 	{
-		Scanner teclado = new Scanner(System.in);
 		System.out.println(text);
 		System.out.println(answer_1 + "\n" + answer_2 + "\n" + answer_3 + "\n" + answer_4 + "\n" + answer_5 + "\n");
 		number = teclado.nextInt();
@@ -26,6 +26,5 @@ public class Methods
 			  Data.points += 100;
 			  Data.combo = 0;
 		  }
-		//teclado.close();
 	}
 }
